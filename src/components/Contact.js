c292ae483ecdd0bf5243c8c1bf1cb9e8834fb4da
Julian Gaston <103215878+JGDevelopments@users.noreply.github.com>
2022-11-13 11:03:15 -0600

    Updated getform endpoint

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/ab8a7f8e-a012-423e-9bb1-c3cdc025747d"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
